d21531a34599fa75509f597041e91b00765c27be
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-10-29 16:36:09 -0400

    New animation for slide without padding and margin

--- src/app/public/slide.ts
+++ src/app/public/slide.ts
@@ -26,3 +26,26 @@ export const skyAnimationSlide = trigger('skyAnimationSlide', [
     animate('150ms ease-in')
   )
 ]) as AnimationEntryMetadata;
+
+export const skyAnimationSlideNoPaddingOrMargin = trigger('skyAnimationSlideNoPaddingOrMargin', [
+  state('down', style({
+    overflow: 'visible',
+    height: '*',
+    paddingTop: '*',
+    paddingBottom: '*',
+    marginTop: '*',
+    marginBottom: '*'
+  })),
+  state('up', style({
+    overflow: 'hidden',
+    height: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginTop: 0,
+    marginBottom: 0
+  })),
+  transition(
+    'up <=> down',
+    animate('150ms ease-in')
+  )
+]) as AnimationEntryMetadata;
